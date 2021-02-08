128dcee7fe2446453143a507718d6f4600920aef
Gruda-adithyan <deepaadithyan56@gmail.com>
2021-02-08 20:36:03 +0530

    .sh File is added to run main.py

---
+++ app.sh
@@ -0,0 +1,17 @@
+#NOTE
+#  THIS FILE WILL ONLY WORK FOR LINUX USERS
+#IF YOU FACE A PYTHON ERROR THEN REPLACE ALL 'python' COMMANDS TO 'python3'
+
+echo "Hello Welcome back Adithyan"
+echo "---------------------------"
+printf "\n"
+
+echo "Loading Your Downloader app"
+
+echo "cd /home/garuda/Documents/Python_Projects/Youtube_Video_Downloader"
+# So user in the below line after the 'cd ' command enter the location where you have store teh app
+# shellcheck disable=SC2164
+cd /home/garuda/Documents/Python_Projects/Youtube_Video_Downloader
+
+echo "python main.py"
+python main.py
