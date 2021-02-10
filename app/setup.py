06039a3161fb568ce54f978b82125f421064baef
Adithyan.A <deepaadithyan56@gmail.com>
2021-02-10 14:46:04 +0530

    New function to get the username of the PC is crated, Changed the location with dynamic username and some unwanted code is deleted

--- app/setup.py
+++ app/setup.py
@@ -1,4 +1,14 @@
 import platform
+import os
+
+def get_user_name(data_dict):
+    """
+    This will get us the username of the system
+    :return: This will return us the Username of the PC
+    """
+    username = os.getenv("username")
+    data_dict['username'] = username
+    return username
 
 
 def setup_os(data_dict):
@@ -8,21 +18,16 @@ def setup_os(data_dict):
     :return: the dict with new value and key is returned
     """
     osname = platform.system()
+    username = get_user_name(data_dict)
     if osname == "Windows":  # For Windows
         data_dict['cmd'] = 'explorer '
-        data_dict['loc'] = 'C:\\Users\\deepa\\Videos\\Youtube_Video_Downloader'
+        data_dict['loc'] = f'C:\\Users\\{username}\\Videos\\Youtube_Video_Downloader'
     elif osname == "Linux":  # For Linux
         data_dict['cmd'] = 'xdg-open '
-        data_dict['loc'] = '/home/garuda/Videos/Youtube_Video_Downloader/'
+        data_dict['loc'] = f'/home/{username}/Videos/Youtube_Video_Downloader/'
     elif osname == "Darwin":  # For Mac
         data_dict['cmd'] = 'Open '
         data_dict['loc'] = ''
     else:
         print("OS Of Your System Is Not Supported By Us")
     return data_dict
-
-
-if __name__ == '__main__':
-    data = {}
-    setup_os(data)
-    print(data)
