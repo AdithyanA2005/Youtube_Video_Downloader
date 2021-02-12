3985b18b943eb545ba97e42bba44883e30de9c16
Adithyan_A <deepaadithyan56@gmail.com>
2021-02-12 21:45:24 +0530

    Shell Files added

---
+++ bin/app.sh
@@ -0,0 +1,21 @@
+echo "Hello Welcome back Adithyan"
+echo "---------------------------"
+printf "\n"
+
+echo "Now I We are going to the location of the main runnable pythonn script"
+echo "cd /home/garuda/Documents/Python_Projects/Youtube_Video_Downloader"
+echo "Loading Your Downloader app"
+cd /home/garuda/Documents/Python_Projects/Youtube_Video_Downloader
+
+echo "Adithyan this is the list of file in your project"
+echo "dir"
+dir
+
+echo "Now we are activating the Virtual_Environment in which you have installed the required python packages"
+echo "source venv/bin/activate"
+source venv/bin/activate
+
+echo "Now we are running the main python file"
+echo "python main.py"
+python main.py
+
