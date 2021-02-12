ce68c1917a05a6cfba8ec19701a0fb4590a413e7
Adithyan_A <deepaadithyan56@gmail.com>
2021-02-12 22:00:35 +0530

    Shell Files updated

--- bin/app.ps1
+++ bin/app.ps1
@@ -1,1 +1,22 @@
-echo "Hello Adithyan"
+echo "Hello Adithyan Welcome Back"
+printf "\n"
+
+echo "Going to your project section"
+echo "cd C:\Users\deepa\Documents\Coding\Python\Projects\Youtube_Video_Downloader"
+cd C:\Users\deepa\Documents\Coding\Python\Projects\Youtube_Video_Downloader
+
+echo "Adithyan This is the list of files in the poject"
+echo "dir"
+dir
+
+echo "Now we are activating the Virtual_Environment In which we have installed the Python Packages"
+echo ".\venv\Scripts\activate"
+.\venv\Scripts\activate
+
+echo "To Keep Your data uptodate on various pc we are getting latest updates from github"
+echo "git pull origin main"
+git pull origin main
+
+"And Finalle Now we are loading the app"
+echo "python main.py"
+python main.py
