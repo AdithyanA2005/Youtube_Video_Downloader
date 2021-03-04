7eaa3c8be4f85a17eadd0e5b672dcf7b26306446
Adithyan.A <deepaadithyan56@gmail.com>
2021-03-04 17:06:40 +0530

    This is the cli version of the project

--- main.py
+++ main.py
@@ -1,113 +1,141 @@
 import os
+import sys
 import time
-from pytube import YouTube
-from PyQt5.QtWidgets import *
-from app.gui import Ui_MainWindow
-from windows.windows_os import Windows
+import platform
+from pytube import YouTube as YT
+from app.settings import data
 # inp_entry = "https://www.youtube.com/watch?v=sE9oZ4fdb1s"
 
 
-class Main(QMainWindow):
-    def __init__(self, usr_data):
+class Main():
+    def __init__(self, cli_values):
         super().__init__()
-        self.data = usr_data
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
-        vid = "video"
-        self.ui.download_video_btn.clicked.connect(self.check_url)
-        self.ui.download_mp3.clicked.connect(self.check_url)
-        self.ui.clear_entry.clicked.connect(self.clear_it)
-        self.ui.destimation_btn.clicked.connect(self.show_downloads)
-        windows.load_gui_change(self)
-        self.show()
+        self.start_app()
 
-    def video_download(self, inp_entry):
-        start_time = time.time()
+
+    def start_app(self):
         try:
-            print("0%")
-            print(inp_entry)
-            print("20%")
-            url = YouTube(str(inp_entry))
-            print("40%")
-            video = url.streams.first()
-            print("60%")
-            download_destination = f"{self.data['loc']}/videos"
-            print("80%")
-            video.download(output_path=download_destination)
-            print("100%")
-            exe_time = time.time() - start_time
-            self.change_pht(f"the file was downloaded in {exe_time} seconds")
+            self.video_url = sys.argv[1]
         except Exception as e:
-            print(e)
-            self.change_pht("error: please ensure that you have entered a valid url")
+            self.video_url = ""
+        try:
+            self.video_name = sys.argv[2]
+        except Exception as e:
+            self.video_name = ""
 
-    def audio_download(self, inp_entry):
+        self.data = data
+        
+        if self.video_name == "":
+            self.video_download(self.video_url, self.video_name)
+
+        elif self.video_name == "mp4":
+            self.video_download(self.video_url, self.video_name)
+
+        elif self.video_name == "video":
+            self.video_download(self.video_url, self.video_name)
+
+        elif self.video_name == "mp3":
+            self.audio_download(self.video_url, self.video_name)
+
+        elif self.video_name == "audio":
+            self.audio_download(self.video_url, self.video_name)
+
+        else:
+            self.video_download(self.video_url, self.video_name)
+        
+        
+
+    def video_download(self, url, name):
+        try:
+            start_time = time.time()
+            url_start = "https://www.youtube.com/watch?v="
+
+            if url_start in url:
+                video_url = str(url)
+                print("0%")
+                print(f"The url you have entered is: \n-> {url}\nThe Name of the Video is: \n->{name}")
+                print("20%")
+                url = YT(video_url) 
+                print("40%")
+                video = url.streams.first()
+                print("50%")
+                download_destination = self.get_location()
+                download_destination = f"{download_destination}videos"
+                print("80%")
+                video.download(output_path=download_destination)
+                print("100%")
+                print(f"The video has been downloaded in the location {download_destination}")
+                exe_time = time.time() - start_time
+                print(f"Process completed in {exe_time}")
+                self.show_downloads(download_destination)
+
+            else:
+                print("This is not a valid URL")
+
+        except Exception as e:
+            print("Stopped in video_download function because: ")
+            print(e)
+
+
+    def audio_download(self, url, name):
         start_time = time.time()
         try:
+            video_url = str(url)
             print("0%")
-            print(inp_entry)
-            print("10%")
-            url = YouTube(str(inp_entry))
+            print(f"The url you have entered is: \n-> {url}\nThe Name of the Video is: \n->{name}")
             print("20%")
+            url = YT(video_url)
+            print("30%")
             video = url.streams.filter(only_audio=True).first()
             print("40%")
-            download_destination = f"{self.data['loc']}/audios"
+            download_destination = self.get_location()
+            download_destination = f"{download_destination}audio"
             print("60%")
             video.download(output_path=download_destination)
             print("80%")
             os.rename(f"{download_destination}/{url.title}.mp4", f"{download_destination}/{url.title}.mp3")
             print("100%")
+            print(f"The audio file has been downloaded in the location {download_destination}")
             exe_time = time.time() - start_time
-            self.change_pht(f"the file was downloaded in {exe_time} seconds")
+            print(f"Process completed in {exe_time}")
+            self.show_downloads(download_destination)
+
         except Exception as e:
+            print("Stopped in audio_download function because: ")
             print(e)
-            self.change_pht("error: please ensure that you have entered a valid url")
 
-    def show_downloads(self):
-        self.change_pht("opening your downloaded videos")
-        try:
-            command = f"{self.data['cmd']}{self.data['loc']}"
-            print(command)
-            os.system(command)
-            self.change_pht("enter the url")
-        except Exception as e:
-            print("error opn opening downloads")
 
-    def change_pht(self, text):
-        self.ui.entry_field.setText("")
-        text = text.upper()
-        self.ui.entry_field.setPlaceholderText(text)
+    def get_location(self):
+        self.operating_system = platform.system()
+        yt_folder = data["loc"]
 
-    def check_url(self):
-        inp_entry = self.ui.entry_field.text()
-        sender_button = self.sender()
-        key = sender_button.text()
-        if inp_entry == "":
-            self.change_pht("please enter the url")
+        if self.operating_system == "Windows":
+            download_destination = f"{yt_folder}\\"
+        elif self.operating_system == "Linux":
+            download_destination = f"{yt_folder}\\"
+            download_destination = download_destination.replace("\\", "/")
         else:
-            if "MP4" in key:
-                self.video_download(inp_entry)
-            elif "MP3" in key:
-                self.audio_download(inp_entry)
-            else:
-                print("Invalid Key")
-
-    def clear_it(self):
-        field = self.ui.entry_field
-        field.setText("")
-        pht = "enter the url of the video that you want to download"
-        field.setPlaceholderText(pht.upper())
+            download_destination = "Youtube_Video_Downloader"
+        return download_destination
 
 
-windows = Windows
+    def show_downloads(self, download_destination):
+        print("PRESS '1' TO SHOW THE DOWNLOAD FOLDER \nPRESS '2' TO EXIT THE PROGRAM")
+        show_bool = str(input("Enter Your Option(1, 2): "))
+        if show_bool == "1": 
+            try:
+                os.system(f"explorer {download_destination}")
+
+            except:
+                os.system(f"xdg-open {download_destination}")
+
+        elif show_bool == "2":
+            sys.exit("Bye sir")
+
+        else:
+            sys.exit("Bye sir")
 
 
 if __name__ == '__main__':
-    import sys
-    from app.settings import data
-    from PyQt5 import QtWidgets
-    
-    app = QtWidgets.QApplication(sys.argv)
-    alpha = Main(data)
-    alpha.show()
-    sys.exit(app.exec_())
+    alpha = Main(sys.argv)
+
